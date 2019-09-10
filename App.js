9b46f683bc74d80d799eb3aa34da237cd7ade483
HomBenPeng <asd5530025@gmail.com>
2019-09-11 02:06:52 +0800

    1. add react-native-beacons-manager 2. link react-native-beacons-manager

--- App.js
+++ App.js
@@ -6,109 +6,51 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, { Component } from 'react'
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  DeviceEventEmitter,
+  Platform
 } from 'react-native';
+import Beacons from 'react-native-beacons-manager'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
+  componentdidmount () {
+    const region = {
+        identifier: '',
+        uuid: 'b5b182c7-eab1-4988-aa99-b5c1517008d9' //四月兄弟裝置UUID
+    }
+
+    if (Platform.OS === 'ios') {
+        Beacons.requestWhenInUseAuthorization()
+        Beacons.startMonitoringForRegion(region)
+        Beacons.startRangingBeaconsInRegion(region)
+        Beacons.startUpdatingLocation()
+    } else {
+        Beacons.detectIBeacons()
+        Beacons
+            .startRangingBeaconsInRegion(region)
+            .then(() => console.log('Beacons ranging started succesfully'))
+            .catch(error => console.log(`Beacons ranging not started, error: ${error}`))
+    }
+
+    // 加入監聽事件接收Becons 訊號
+    DeviceEventEmitter.addListener(
+        'beaconsDidRange',
+        (data) => {
+            console.log(JSON.stringify(data))
+        }
+    )
+  }
+
+  render () {
+    return(
+      <View>
+        <Text>asd</Text>
+      </View>
+    )
+  };
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
 export default App;
